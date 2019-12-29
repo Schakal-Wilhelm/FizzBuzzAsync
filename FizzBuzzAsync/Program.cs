0f26e95472d4f19f7751d093f6529a7116018abe
Schakal-Wilhelm <35175140+Schakal-Wilhelm@users.noreply.github.com>
2019-12-29 22:43:01 +0100

    Add files via upload

---
+++ FizzBuzzAsync/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace FizzBuzzAsync
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            RunFizzBuzzAsync();
+            Console.Read();
+        }
+
+        public static async void RunFizzBuzzAsync()
+        {
+            await Task.Run(() =>
+            {
+                for (int i = 1; i <= 50; i++)
+                {
+                    Console.Write(i + ".");
+
+                    if (i%3 == 0)
+                        Console.Write("Fizz");
+
+                    if (i%5 == 0)
+                        Console.Write("Buzz");
+
+                    Console.WriteLine();
+                }
+            });
+        }
+    }
+}
